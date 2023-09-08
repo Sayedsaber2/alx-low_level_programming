88b4d716cbdce37a1c77c4af268e42b128ec04cf
sayed saber <sayedsaber@gmail.com>
2023-09-07 18:44:16 -0700

    my frist commit4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main - entry point
- * Description:A C program that prints with put Functions
- * return: always 0 (success)
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
