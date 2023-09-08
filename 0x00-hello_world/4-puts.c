73e1c81bedd51d94c43352aa9e958f56626ed645
sayed saber <sayedsaber@gmail.com>
2023-09-07 18:41:29 -0700

    my frist commit4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 /**
  * main - entry point
- * Description:a C program that prints with put Functions
+ * Description:A C program that prints with put Functions
  * return: always 0 (success)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
-
 }
