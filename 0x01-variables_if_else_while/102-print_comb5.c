4a8e951bdd738ef4c447a23fd1738c6a335950b3
sayed saber <sayedsaber@gmail.com>
2023-09-10 00:56:32 -0700

    To some extent I understand11

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,38 @@
+#include <stdio .h >
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int digit1 = 0, diigt2;
+
+	while (digit1 <= 99)
+	{
+		digit1 = digit2;
+			while (digit2 <= 99)
+			{
+				if (digit1 != gigit2)
+				{
+					putchar((digit1 / 10) + 48);
+					putchar((digit1 % 10) + 48);
+					putchar(' ');
+					putchar((digit2 / 10) + 48);
+					putchar((digit2 % 10) + 48);
+
+					if (digit1 != 98 || digit != 98)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				digit2++;
+			}
+			digit1++;
+	}
+	putchar('\n')
+		return (0);
+}
+
