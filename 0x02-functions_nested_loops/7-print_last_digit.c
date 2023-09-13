5b1f71d54f6a481fecde4e755b866c9d3c958147
sayed saber <sayedsaber@gmail.com>
2023-09-13 16:50:46 -0700

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *  print_last_digit - prints the last digit of a number
+ *  @n:task numper input
+ *  Return: lastdigit
+ */
+int print_last_digit(int n)
+	int lastdigit;
+{
+	if (n < 0)
+		lastdigit = -1 * (n % 10);
+	else
+		lastdigit = % 10;
+	_putcar(lastdigit + '0');
+	return (lastdigit);
+}
