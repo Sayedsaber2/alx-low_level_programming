157a4b4f0d8deb08f39d9c30386e2c2c6ccdc252
sayed saber <sayedsaber@gmail.com>
2023-09-14 01:15:31 -0700

    this commit22

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,38 +1,50 @@
-#include "main.h"
+nclude "main.h"
+
 /**
- * print_times_table -  prints the n times table
- * @n: task numper input
+ * print_times_table - Print the n times table, starting from 0
+ * @n: The times table to print
+ *
+ * Return: Nothing
  */
 void print_times_table(int n)
 {
-	int num, mult, prod;
+	int x, y, mult, one, ten, hund;
 
-	if (n <= 15 && n >= 0)
+	x = 0;
+	if (n >= 0 && n <= 15)
 	{
-		for (num = 0; num <= n; num++)
+		while (x <= n)
 		{
-			_putchar(48);
-			for (mult = 1; mult <= 9; mult++)
+			y = 0;
+			while (y <= n)
+			{
+				mult = x * y;
+				one = mult % 10;
+				ten = mult % 100 / 10;
+				hund = mult / 100;
+				if (hund == 0 && y != 0)
 				{
-				_putchar(',');
-				_putchar(' ');
-
-				prod = num * mult;
-
-				if (prod <= 9)
 					_putchar(' ');
-				if (prod <= 99)
-					_putchar(' ');
-				if (prod <= 100)
+					if (ten == 0)
+						_putchar(' ');
+					else
+						_putchar(ten + '0');
+				}
+				else if (hund != 0)
 				{
-					_putchar((prod / 100) + 48);
-					_putchar((prod % 10) % 10 + 48);
-				} else if (prod <= 99 && prod >= 10)
-					_putchar((prod / 10) + 48);
-					((prod % 10) + 48);
+					_putchar(hund + '0');
+					_putchar(ten + '0');
+				}
+				_putchar(one + '0');
+				if (y != n)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+				y++;
 			}
 			_putchar('\n');
+			x++;
 		}
 	}
 }
-
